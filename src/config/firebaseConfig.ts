c7e89c883b5e1101a6609708313777c95489de59
Valmos22 <57192775+Valmos22@users.noreply.github.com>
2024-09-15 16:55:01 -0500

    Update firebaseConfig.ts

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -1,6 +1,18 @@
 import admin from 'firebase-admin';
-import serviceAccount  from './serviceAccountKey.json';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY environment variable is not defined.");
+}
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+console.log('Service Account:', serviceAccount);
+
+if (!serviceAccount.client_email || !serviceAccount.private_key) {
+    throw new Error("FIREBASE_SERVICE_ACCOUNT_KEY environment variable is missing required fields.");
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
